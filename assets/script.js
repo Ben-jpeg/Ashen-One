37670dfcfbff381d715c8198aaabd8859f3613dd
Bendjade BAROINI <72921912+Ben-jpeg@users.noreply.github.com>
2024-09-06 00:26:08 +0200

    adds

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,36 @@
+/* Arrow pour revenir en debut de page* ------------------------------------*/
+const up = document.querySelector('.up');
+
+up.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
+/* FIN ARRON FIN ARROW FIN ARROW FIN ARROW FIN ARROW FIN ARROW FIN ARROW----*/
+
+
+
+
+/* ScrollReveal* -------------------------------------------------------*/
+ScrollReveal().reveal('.listeAnimes', { delay: 300 });
+ScrollReveal().reveal('.pics', { delay: 400 })
+ScrollReveal().reveal('.sousTitre', { delay: 400 })
+/* ScrollReveal FIN SCROLLREVEAL FIN SCROLLREVEAL FIN SCROLLREVEAL FIN--*/
+
+
+
+
+/* Modale du burger* --------------------------------------------------------*/
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+/* FIN MODALE FIN MODALE FIN MODALE FIN MODALE FIN MODALE FIN MODALE FIN MODALE*/
+
+
